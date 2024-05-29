45e5f3ad4f2c75c0f6b8acdb25b3ffd41b7cf1c6
samrodrigues <sam@olivers.tech>
2024-05-29 19:28:43 -0300

    making it cute

--- app.vue
+++ app.vue
@@ -3,3 +3,9 @@
     <RouterView />
   </div>
 </template>
+<style>
+body {
+  padding: 0;
+  margin: 0;
+}
+</style>
--- pages/terminal.vue
+++ pages/terminal.vue
@@ -1,9 +1,12 @@
 <template>
   <main>
-    <div v-for="message in messages">
+    <div class="message" v-for="message in messages">
       {{ message.content }}
     </div>
-    <input type="text" v-model="newMessage" @keyup.enter="postMessage">
+    <span class="caret-block">&gt;</span><input class="caret-block" :class="{'submitted blink': isSubmitting}" type="text" v-model="newMessage" @keyup.enter="postMessage">
+    <div class="loader message" v-if="isSubmitting">
+      <span class="loader__dot">.</span><span class="loader__dot">.</span><span class="loader__dot">.</span>
+    </div>
   </main>
 </template>
 <script>
@@ -16,7 +19,8 @@ export default {
     return {
       implementation: "",
       newMessage: "",
-      messages: [],
+      isSubmitting: false,
+      messages: [{"content": "CERBERUS v2322.1"}],
     }
   },
   mounted() {
@@ -24,7 +28,9 @@ export default {
   },
   methods: {
     async postMessage () {
+      this.isSubmitting = true;
       const {data} = await axios.post(`https://cerberus-vsm8.onrender.com/api/implementations/${this.implementation}`, {message: this.newMessage})
+      this.isSubmitting = false;
       this.messages.push({"content": this.newMessage})
       this.messages.push({"content": data?.response?.[0]?.[1] || 'bad response'})
       this.newMessage = '';
@@ -32,6 +38,50 @@ export default {
   }
 }
 </script>
-<style scoped>
+<style scoped lang="scss">
+main {
+  background-color: #282c34;
+  color: #ededed;
+  font-family: monospace;
+  padding: 24px 24px 48px;
+}
+input {
+  background-color: transparent;
+  padding-left: 6px;
+  color: #ededed;
+  border: none;
+  outline: none;
+  caret-color: #dedede;
+  position: relative;
+  caret-shape: block;
+  font-family: monospace;
+  margin-bottom: 6px;
+}
+input.submitted {
+  color: #878787;
+  caret-color: transparent;
+}
+pre {
+  padding: 0;
+}
+.caret-block > span {
+  background-color: hsla(1, 1%, 100%, 1);
+  color: #000;
+}
 
+@keyframes blink {50% { color: transparent }}
+.loader__dot { animation: 1s blink infinite }
+.loader__dot:nth-child(2) { animation-delay: 250ms }
+.loader__dot:nth-child(3) { animation-delay: 500ms }
+
+
+input:focus, input:active {
+  outline: none;
+}
+.message {
+  margin-bottom: 6px;
+}
+.message:before {
+  content: '> ';
+}
 </style>
